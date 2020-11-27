93af96cd61305adb5a4664e5bed359ee6207a410
zyd16888 <zyd475182659@163.com>
2020-11-27 16:14:15 +0800

    update 11.27

---
+++ source/_posts/test/python编码问题.py
@@ -0,0 +1,18 @@
+---
+title: python编码问题
+copyright: true
+abbrlink: ed6658a4
+notshow: false
+description: 'locale.getpreferredencoding'
+image:
+  - 'https://data.singlelovely.cn/CoverPicture/ed6658a4.svg!CoverPicture'
+tags:
+  - python
+categories: python
+date: 2020-10-27 10:37:00
+password:
+photos:
+sticky:
+---
+
+UnicodeDecodeError: 'gbk' codec can't decode byte 0x80 in position 34: illegal multibyte sequence
